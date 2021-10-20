aad31c9011a44ce2632096223e4404d1f6b13039
galuropek <Z322200119i>
2021-10-20 11:03:00 +0300

    fix logic

--- app.rb
+++ app.rb
@@ -1,29 +1,28 @@
-require_relative 'time_formatter'
+require_relative "time_formatter"
 
 class App
-  AVAILABLE_PARAMS = %w[year month day hour minute second].freeze
-
   def call(env)
     request = Rack::Request.new(env)
 
-    case request.path_info
-    when /time/
-      request_query = URI.decode_www_form(request.query_string).to_h
-      format_params = request_query["format"]
-      return response(404, "Not found format params!") unless format_params
-
-      params = format_params.split(',').map(&:strip).map(&:downcase).uniq
-      if (unknown_params = params - AVAILABLE_PARAMS).empty?
-        response(200, TimeFormatter.new(params).time)
-      else
-        response(400, "Unknown time format #{unknown_params}!")
-      end
+    if request.path_info =~ /time/
+      time_formatter = TimeFormatter.new(request.query_string)
+      prepare_time_response(time_formatter)
     else
       response(404, "Not found path \"#{request.path_info}\"!")
     end
   end
 
   private
+
+  def prepare_time_response(time_formatter)
+    if time_formatter.success?
+      response(200, time_formatter.time_string)
+    elsif time_formatter.format_params_present?
+      response(400, time_formatter.invalid_string)
+    else
+      response(400, time_formatter.no_format_params_string)
+    end
+  end
 
   def response(code, body)
     [code, headers, ["#{body}\n"]]
--- time_formatter.rb
+++ time_formatter.rb
@@ -8,11 +8,43 @@ class TimeFormatter
     'second' => '%S'
   }.freeze
 
-  def initialize(params)
-    @params = params
+  def initialize(query_string)
+    @query_string = query_string
   end
 
-  def time
-    Time.now.strftime @params.map { |param| TIME_FORMAT[param] }.join('-')
+  def time_string
+    Time.now.strftime(time_params.map { |param| TIME_FORMAT[param] }.join('-'))
+  end
+
+  def invalid_string
+    "Unknown time format #{unknown_params}!"
+  end
+
+  def no_format_params_string
+    "Not found format params!"
+  end
+
+  def success?
+    return false unless format_params_present?
+
+    unknown_params.empty?
+  end
+
+  def format_params_present?
+    format_params && !format_params.empty?
+  end
+
+  private
+
+  def time_params
+    @time_params ||= @format_params.split(',').map(&:strip).map(&:downcase).uniq
+  end
+
+  def format_params
+    @format_params ||= URI.decode_www_form(@query_string).to_h["format"]
+  end
+
+  def unknown_params
+    @unknown_params ||= time_params - TIME_FORMAT.keys
   end
 end
